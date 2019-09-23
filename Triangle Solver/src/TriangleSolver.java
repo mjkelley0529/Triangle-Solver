81a459a872401ba1393464ac55871178321e1c37
mkelley <mkelley@LAPTOP-MRG78R6F>
2019-09-23 10:03:17 -0500

    Current Version as of 23Sep2019

--- Triangle Solver/src/TriangleSolver.java
+++ Triangle Solver/src/TriangleSolver.java
@@ -11,39 +11,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class TriangleSolver extends JFrame implements ActionListener {
+public class TriangleSolver extends JFrame implements ActionListener, KeyListener {
 	//Static Variables
 	private static final long serialVersionUID = -4198600644269193361L;
 	private static final int frameH=400, frameW=600;
 	private static final String displayString[]= {"Side A=", "Angle A=", "Side B=","Angle B=","Side C=","Angle C="}, 
-			DEFIN="0", TITLE="Triangle Solver", VERID="Pre-Alpha 0.1";
+			DEFIN="", TITLE="Triangle Solver", VERID="Pre-Alpha 0.1";
 	//Swing/UI Variables
 	private JTextArea[] inputs = new JTextArea[6], displays=new JTextArea[6];
-	private JButton reset=new JButton("Reset");
-	private JPanel pane=new JPanel(), spacers[]=new JPanel[23];
-	private GridLayout mainLay=new GridLayout(5,7,10,10);
-	private KeyListener kL=new KeyListener() {
-		@Override
-		public void keyTyped(KeyEvent e) {
-			//System.out.println("Typed: "+e.getKeyChar());
-		}//keyTyped
-		@Override
-		public void keyPressed(KeyEvent e) {
-			//System.out.println("Pressed: "+e.getKeyChar());
-		}//keyPressed
-		@Override
-		public void keyReleased(KeyEvent e) {
-			//System.out.println("Released: "+e.getKeyChar());
-			if(intCheck(e.getKeyChar())) {
-				for(int i=0;i<inputs.length;i++) {
-					inputBool[i]=intCheck(inputs[i].getText());
-				}//for
-				if(logicRunnable()&&!logicRan) {
-					runLogic();
-				}//if
-			}//if
-		}//keyReleased
-	};//KeyListener dec
+	private JButton reset=new JButton("Reset"), enter=new JButton("Run");
+	private JPanel pane=new JPanel(), spacers[]=new JPanel[21];
+	private GridLayout mainLay=new GridLayout(5,7,0,10);
+
 	//Functional Variables
 	private boolean inputBool[]= {false,false,false,false,false,false}, logicRan=false;
 
@@ -54,14 +33,13 @@ public class TriangleSolver extends JFrame implements ActionListener {
 		setTitle(TITLE+", "+VERID);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		addKeyListener(kL);
 
 		pane.setLayout(mainLay);
 
 		for(int i=0;i<inputs.length;i++) {
 			inputs[i]=new JTextArea();
 			inputs[i].setText(DEFIN);
-			inputs[i].addKeyListener(kL);
+			inputs[i].addKeyListener(this);
 		}
 		for(int i=0;i<displays.length;i++) {
 			displays[i]=new JTextArea();
@@ -73,7 +51,10 @@ public class TriangleSolver extends JFrame implements ActionListener {
 			spacers[i]=new JPanel();
 			spacers[i].setFocusable(false);
 		}
+		enter.addActionListener(this);
+		enter.addKeyListener(this);
 		reset.addActionListener(this);
+		reset.addKeyListener(this);
 		for(int i=0;i<8;i++)
 			pane.add(spacers[i]);
 		pane.add(displays[0]);
@@ -95,23 +76,24 @@ public class TriangleSolver extends JFrame implements ActionListener {
 		pane.add(spacers[14]);
 		pane.add(displays[5]);
 		pane.add(inputs[5]);
-		for(int i=15;i<21;i++)
+		for(int i=15;i<20;i++)
 			pane.add(spacers[i]);
+		pane.add(enter);
 		pane.add(reset);
-		pane.add(spacers[21]);
+		pane.add(spacers[20]);
 
 		add(pane);
 		setVisible(true);
 	}//init
 	private void runLogic() {
-		for(JTextArea i:inputs)
+		/*for(JTextArea i:inputs)
 			i.setEditable(false);
-		logicRan=true;
+		logicRan=true;*/
 	}//runLogic
 	private void reset() {
 		logicRan=false;
 		for(int i=0;i<inputs.length;i++) {
-			inputs[i].setText("0");
+			inputs[i].setText(DEFIN);
 			inputBool[i]=false;
 			inputs[i].setEditable(true);
 		}//for
@@ -129,32 +111,8 @@ public class TriangleSolver extends JFrame implements ActionListener {
 	}//main
 
 	//Variable Methods
-	private boolean intCheck(String checkString) {
-		boolean returnValue=false;
-		if(checkString.length()==0||(checkString.length()==1&&checkString.contains("0"))) {
-			return returnValue;
-		}//if
-		for(int i=0;i<checkString.length();i++) {
-			switch(checkString.charAt(i)) {
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
-			case '0':
-				returnValue=true;
-				break;
-			default:
-				return false;
-			}//switch
-		}//for
-		return returnValue;
-	}//intCheck(String)
 	private boolean intCheck(char checkChar) {
+		System.err.println(checkChar);
 		boolean returnValue=false;
 		switch(checkChar) {
 		case '1':
@@ -174,32 +132,56 @@ public class TriangleSolver extends JFrame implements ActionListener {
 		}//switch
 		return returnValue;
 	}//intCheck(char)
+	private boolean intCheck(String checkString) {
+		boolean returnValue=false;
+		if(checkString.length()==0) {
+			return returnValue;
+		}//if
+		for(int i=0;i<checkString.length();i++) {
+			if(intCheck(checkString.charAt(i))) {
+				returnValue=true;
+			} else {
+				returnValue=false;
+				break;
+			}
+		}//for
+		return returnValue;
+	}//intCheck(String)
 	private boolean logicRunnable() {
 		boolean returnValue;
 		int trueCount=0;
-		for(boolean i:inputBool)
+		for(boolean i:inputBool) {
+			System.err.println(i);
 			if(i)
 				trueCount++;
-		if(trueCount==3&&!(inputBool[3]&&inputBool[4]&&inputBool[5])) {
+		}
+		boolean A3=inputBool[1]&&inputBool[3]&&inputBool[5], S3=inputBool[0]&&inputBool[2]&&inputBool[4];
+		if(!A3&&!S3&&trueCount>=3) {
 			returnValue=true;
-		} else if (inputBool[3]&&inputBool[4]&&inputBool[5]) {
-			double inputNums[]= {Double.parseDouble(inputs[3].getText()),Double.parseDouble(inputs[4].getText()),Double.parseDouble(inputs[5].getText())};
+			System.err.println("Case 1 True");
+		} else if (A3) {
+			double inputNums[]= {Double.parseDouble(inputs[1].getText()),Double.parseDouble(inputs[3].getText()),Double.parseDouble(inputs[5].getText())};
+			System.err.println("Case 2");
 			if(inputNums[0]+inputNums[1]+inputNums[2]==180) {
 				returnValue=true;
+				System.err.println("Case 2 True");
 			} else {
 				returnValue=false;
-				System.err.println("Angles do not equal 180 degrees.");
+				System.err.println("Case 2 False: Angles do not equal 180 degrees.");
 			}
-		} else if (inputBool[0]&&inputBool[1]&&inputBool[2]) {
-			double inputNums[]= {Double.parseDouble(inputs[0].getText()),Double.parseDouble(inputs[1].getText()),Double.parseDouble(inputs[2].getText())};
+		} else if (S3) {
+			System.err.println("Case 3");
+			double inputNums[]= {Double.parseDouble(inputs[0].getText()),Double.parseDouble(inputs[2].getText()),Double.parseDouble(inputs[4].getText())};
 			double sortedNums[]=sort(inputNums);
-			if(sortedNums[2]>sortedNums[0]+sortedNums[1]) {
+			if(sortedNums[2]<=sortedNums[0]+sortedNums[1]) {
+				System.err.println("Case 3 True");
 				returnValue=true;
 			} else {
 				returnValue=false;
-				System.err.println("Given side lengths cannot form a triange.");
+				System.err.println("Case 3 False: Given side lengths cannot form a triange.");
 			}
 		} else {
+			System.err.println("Case 1 False");
 			returnValue=false;
 		}
 		return returnValue;
@@ -209,6 +191,26 @@ public class TriangleSolver extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent evt) {
 		if(evt.getSource()==reset) {
 			reset();
-		}//if
+		}
+		if(evt.getSource()==enter&&logicRunnable()&&!logicRan) {
+			runLogic();
+		}
 	}//actionPerformed
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}//keyTyped
+	@Override
+	public void keyPressed(KeyEvent e) {
+	}//keyPressed
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_ENTER&&logicRunnable()&&!logicRan) {
+				enter.doClick();
+		} else {
+			for(int i=0;i<inputs.length;i++) {
+				System.err.println("\""+inputs[i].getText()+"\"");
+				inputBool[i]=intCheck(inputs[i].getText().toLowerCase());
+			}//for
+		}//if
+	}//keyReleased
 }//class
